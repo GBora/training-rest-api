1809d39a25e5b01ca400ebcd64d31492422428f5
George Bora <gpbora@gmail.com>
2022-09-07 12:03:11 +0300

    remove authorization

--- personal_computers/updatePersonalComputer.js
+++ personal_computers/updatePersonalComputer.js
@@ -2,18 +2,18 @@ const { getPCs, savePCsList } = require("../data/pcRepository");
 
 const updatePersonalComputer = async (req, res) => {
     try {
-        const bearerToken = req.headers['authorization'];
-        const validUser = await isValidUser(bearerToken.split(' ')[1]);
+        // const bearerToken = req.headers['authorization'];
+        // const validUser = await isValidUser(bearerToken.split(' ')[1]);
 
         const id = req.params.id;
         const list = await getPCs();
         const index = list.findIndex(pc => pc.id === id);
         console.log(index);
     
-        if (!validUser) {
-            console.error('not valid');
-            res.status(404).end();
-        }
+        // if (!validUser) {
+        //     console.error('not valid');
+        //     res.status(404).end();
+        // }
 
         if (!index) {
             res.status(404).end();
